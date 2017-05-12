7e1694cdec869b20ab29e56c04844942f2b38683
BHasani <abdul.kadir.hasani@basajans.com>
2017-05-13 00:58:09 +0700

    oke

---
+++ src/main/java/com/bebaryan/b/core/model/PersonModel.java
@@ -0,0 +1,74 @@
+package com.bebaryan.b.core.model;
+
+/**
+ *
+ * @author hasani
+ */
+public class PersonModel {
+    
+    private String requestId;
+    private String fullName;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private Integer age;
+
+    public PersonModel() {
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+    
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return requestId;
+    }
+
+   
+    
+}
